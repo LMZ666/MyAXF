49507935ab1e0b9ccd2a6e6ebb1239ef2a30cc3f
LMZ666 <lmzqqyouxiang@qq.com>
2018-11-03 15:31:19 +0800

    引进bootstrap

--- App/urls.py
+++ App/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("cart/", views.cart, name="cart"),
     path("mine/", views.mine, name="mine"),
-    path("market/", views.market, name="market")
+    path("market/", views.market, name="market"),
+    # 此处的参数要和函数的参数名称相同
+    path("market/<int:categoryid>/", views.market, name="market1"),
+    path("market/<int:categoryid>/<int:childtypeid>/",views.market,name="market2"),
+    path("market/<int:categoryid>/<int:childtypeid>/<int:sorttype>/",views.market,name="market3")
+
 ]
--- App/views.py
+++ App/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from App.models import Wheel, Nav, Mustbuy, Shop, MainShop
+from App.models import Wheel, Nav, Mustbuy, Shop, MainShop, Foodtypes, Goods
 
 
 def home(request):
@@ -41,6 +41,7 @@ def base(request):
 
 
 def cart(request):
+
     return render(request, "cart/cart.html")
 
 
@@ -48,5 +49,38 @@ def mine(request):
     return render(request, "mine/mine.html")
 
 
-def market(request):
-    return render(request, "market/market.html")
+def market(request,categoryid=0,childtypeid=0,sorttype=0):
+    foodtypes = Foodtypes.objects.all()
+    if categoryid == 0:
+        categoryid = foodtypes.first().typeid
+    foodtype = Foodtypes.objects.get(typeid=categoryid)
+    # 当我们传递两个有关联的值时，可以使用数组嵌套字典的形式
+    # 这样方便了在模板中取值
+    typeidnamelist=list()
+    for type in foodtype.childtypenames.split("#"):
+        typeidname={
+            "typeid":type.split(":")[1],
+            "typename":type.split(":")[0]
+        }
+        typeidnamelist.append(typeidname)
+
+    # 商品的筛选
+    if childtypeid==0:
+        goods =Goods.objects.filter(categoryid=categoryid)
+    else:
+        goods = Goods.objects.filter(categoryid=categoryid,childcid=childtypeid)
+    if sorttype==2:
+        goods = goods.order_by("productnum")
+    elif sorttype==3:
+        goods = goods.order_by("price")
+    elif sorttype==4:
+        goods = goods.order_by("-price")
+
+    data = {
+        "foodtypes": foodtypes,
+        "goods": goods,
+        "categoryid":categoryid,
+        "typeidnamelist":typeidnamelist,
+        "childtypeid":childtypeid,
+    }
+    return render(request, "market/market.html", context=data)
